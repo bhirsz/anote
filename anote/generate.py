0c66ed901d365781c4a798db191f8f9f1a6333e8
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2023-07-31 18:13:28 +0200

    Add ReleaseNote class for generating release notes from fragments and template

--- anote/generate.py
+++ anote/generate.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 from pathlib import Path
 from typing import List, Optional
 
@@ -10,9 +11,33 @@ from jinja2 import Template
 class ReleaseFragment:
     def __init__(self, path, order, section, extension):
         self.path = path
-        self.order = order
+        self.order = int(order)
         self.section = section
         self.extension = extension
+
+    def load_fragment(self):
+        with open(self.path) as fp:
+            return fp.read()
+
+
+class ReleaseNote:
+    def __init__(
+        self, version: str, fragments: List[ReleaseFragment], template: Template
+    ):
+        self.version = version
+        self.fragments = fragments
+        self.template = template
+
+    def get_template_env(self):
+        sections = defaultdict(list)
+        for fragment in self.fragments:
+            sections[fragment.section].append(fragment)
+        for section, fragments in sections.items():
+            sections[section] = sorted(fragments, key=lambda x: x.order)
+        return {"version": self.version, **sections}
+
+    def generate(self):
+        return self.template.render(**self.get_template_env())
 
 
 def get_template_from_path(template: Optional[str], template_directory: Path) -> str:
@@ -26,7 +51,7 @@ def get_template_from_path(template: Optional[str], template_directory: Path) ->
         return fp.read()
 
 
-def load_template(template: str, template_directory: Path = None) -> str:
+def load_template(template: str, template_directory: Path = None) -> Template:
     return Template(get_template_from_path(template, template_directory))
 
 
@@ -51,7 +76,9 @@ def get_release_fragments(unreleased_path: Path) -> List[ReleaseFragment]:
 
 def generate_release(version: str, unreleased_path: Path, template):
     print(f"Generating release notes for {version}")
-    template = get_template_from_path(template, unreleased_path)
+    template = load_template(template, unreleased_path)
     fragments = get_release_fragments(unreleased_path)
-    # later in template we can refer to fixes, features as list of strings (simple as it is)
+    release_notes = ReleaseNote(version, fragments, template)
+    with open(f"{version}.rst", "w") as fp:
+        fp.write(release_notes.generate())
     # --clear-notes to remove *.rst
